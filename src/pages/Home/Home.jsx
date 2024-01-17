803d537bfd6d47452dfa5d360f0b20865802f696
hridayroy19 <hrhridoyroy503@gmail.com>
2024-01-17 13:07:05 +0600

    add home page features section and rensponsive design

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import Features from "../../components/features/Features";
 import Review from "../../components/homecomponents/review/review";
 
 const Home = () => {
@@ -5,6 +6,10 @@ const Home = () => {
         <div>
             <h1>Home Page</h1>
             <p>import all home components from homecomponents folder </p>
+
+             <div>
+                <Features></Features>
+             </div>
             <Review></Review>
         </div>
     );
