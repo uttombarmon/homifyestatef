b644fa200471527a7045f1b3193b10462f6c7ec0
Irfankhan20 <sipathan96@gmail.com>
2024-01-17 18:20:48 +0600

    some problem

---
+++ src/components/Star/Star.jsx
@@ -0,0 +1,29 @@
+import { FaStar, FaStarHalfAlt } from "react-icons/fa";
+import {AiOutlineStar} from "react-icons/ai";
+
+const Star = ({stars}) => {
+   const ratingStar = Array.from({length: 5}, (elem, index)=>{
+        let number= index + 0.5;
+
+        return (
+            <div>
+                <span className="" key={index}>
+                    {
+                        stars >= index + 1 ?( <FaStar className="text-2xl  text-orange-300"/>) :
+                        stars >= number ? (<FaStarHalfAlt className="text-2xl text-orange-300"/>) :
+                        (<AiOutlineStar className="text-2xl text-orange-300"/>)
+                    }
+                </span>
+            </div>
+        );
+    });
+
+    return (
+        <div>
+            {ratingStar}
+        </div>
+    )
+    
+};
+
+export default Star;
