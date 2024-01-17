8216c79c2d8b5fd14ae58745894f129736bff6ac
mdsoponhossain <mdsoponhossain2388@gmail.com>
2024-01-17 10:34:44 +0600

    home page banner section added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Banner from "../../components/homecomponents/banner/Banner";
 import Review from "../../components/homecomponents/review/review";
 
 const Home = () => {
     return (
         <div>
             <h1>Home Page</h1>
+            <Banner></Banner>
             <p>import all home components from homecomponents folder </p>
             <Review></Review>
         </div>
