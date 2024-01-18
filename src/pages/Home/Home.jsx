be637f3b82839e738257786362201eb8d3826b71
mdsoponhossain <mdsoponhossain2388@gmail.com>
2024-01-18 10:39:54 +0600

    home-page dummy text removed ----- done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,9 +6,7 @@ import Review from "../../components/homecomponents/review/review";
 const Home = () => {
     return (
         <div>
-            <h1>Home Page</h1>
             <Banner></Banner>
-            <p>import all home components from homecomponents folder </p>
             <div>
                 <Features></Features>
             </div>
