fc485bdb7191bb32d9c8eceee00f7c8128004fef
Irfankhan20 <sipathan96@gmail.com>
2024-01-18 17:59:05 +0600

    choose section complete of home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Features from "../../components/features/Features";
+import Choose from "../../components/homecomponents/choose/Choose";
 import LetesNews from "../../components/homecomponents/letesNews/LetesNews";
 import Review from "../../components/homecomponents/review/review";
 
@@ -9,6 +10,7 @@ const Home = () => {
                 <Features></Features>
             </div>
             <LetesNews></LetesNews>
+            <Choose></Choose>
             <Review></Review>
         </div>
     );
