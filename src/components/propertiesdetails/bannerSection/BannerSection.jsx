653310bd5e6946e5e8ec2266445ffddbfa993521
mdsoponhossain <mdsoponhossain2388@gmail.com>
2024-02-23 11:33:58 +0600

    autoplay slider updated

--- src/components/propertiesdetails/bannerSection/BannerSection.jsx
+++ src/components/propertiesdetails/bannerSection/BannerSection.jsx
@@ -20,12 +20,12 @@ export default function BannerSection() {
     const progressContent = useRef(null);
     const onAutoplayTimeLeft = (s, time, progress) => {
         progressCircle.current.style.setProperty('--progress', 1 - progress);
-        progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+        progressContent.current.textContent = `${Math.ceil(time / 10000)}s`;
     };
 
     const [images, setImages] = useState([])
     useEffect(() => {
-        fetch('../../../../public/detailsBanner.json')
+        fetch('/detailsBanner.json')
             .then(res => res.json())
             .then(data => {
                 console.log(data);
@@ -34,11 +34,12 @@ export default function BannerSection() {
     }, [])
     return (
         <>
-            <Swiper style={{ height: '500px' }}
+            <Swiper  style={{ height: '500px'}}
                 spaceBetween={30}
                 centeredSlides={true}
+                ref={progressCircle}
                 autoplay={{
-                    delay: 2500,
+                    delay: 2000,
                     disableOnInteraction: false,
                 }}
                 loop={true}
@@ -48,8 +49,8 @@ export default function BannerSection() {
                 navigation={true}
                 modules={[Autoplay, Pagination, Navigation]}
                 onAutoplayTimeLeft={onAutoplayTimeLeft}
-                className="mySwiper"
-            >
+                className="mySwiper autoplay-progress"
+            > <span ref={progressContent}></span>
                 {
                     images?.map((img,inx)=><SwiperSlide key={inx}><img style={{height:'100%',width:'100%'}} src={img.image} alt="" /></SwiperSlide>)
                 }
